51bcf99aa2b03dd03a8ccfc7c87baa140afee710
tsubasahonda <myaaaicey@gmail.com>
2020-10-13 23:10:47 +0900

    fix: remove unused counter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { Counter } from "./components/Counter";
 import { Mountains } from "./components/Mountains";
 import { Countries } from "./components/Countries";
 export const App = () => {
   return (
     <div class="body">
       <img alt="Vue logo" src="/logo.png" />
-      <div class="counter__wrapper">
-        <Counter />
-      </div>
       <Countries />
       <Mountains />
     </div>
